18112dfd235fcdf984d6ffee3532ed3ac1d57789
fresheneesz <bitetrudpublic@gmail.com>
2013-08-25 23:46:36 -0700

    first commit

---
+++ asyncFuture.js
@@ -0,0 +1,251 @@
+/* Copyright (c) 2013 Billy Tetrud - Free to use for any purpose: MIT License*/
+
+var trimArgs = require("trimArguments")
+
+// todo:
+// * timeout
+// * Long stack traces
+
+module.exports = Future
+
+function Future() {
+    this.resolved = false
+    this.queue = []
+}
+
+// static methods
+
+// has one parameter: either a bunch of futures, or a single array of futures
+// returns a promise that resolves when one of them errors, or when all of them succeeds
+Future.all = function() {
+    if(arguments[0] instanceof Array) {
+        var futures = arguments[0]
+    } else {
+        var futures = trimArgs(arguments)
+    }
+
+    var f = new Future()
+    var results = []
+
+    if(futures.length > 0) {
+        var current = futures[0]
+        futures.forEach(function(future, index) {
+            current = current.then(function(v) {
+                results[index] = v
+                return futures[index+1]
+            })
+        })
+
+        //if
+        current.catch(function(e) {
+            f.throw(e)
+        })
+        // else
+        current.then(function() {
+            f.return(results)
+        })
+
+
+    } else {
+        f.return(results)
+    }
+
+    return f
+}
+
+// either used like futureWrap(function(){ ... })(arg1,arg2,etc) or
+//  futureWrap(object, 'methodName')(arg1,arg2,etc)
+Future.wrap = function() {
+    // function
+    if(arguments.length === 1) {
+        var fn = arguments[0]
+        var object = undefined
+
+
+    // object, function
+    } else {
+        var object = arguments[0]
+        var fn = object[arguments[1]]
+    }
+
+	return function() {
+		var args = Array.prototype.slice.call(arguments)
+		var future = new Future
+		args.push(future.resolver())
+		var me = this
+        if(object) me = object
+        fn.apply(me, args)
+		return future
+	}
+}
+
+
+// default
+var unhandledErrorHandler = function(e) {
+    setTimeout(function() { //  nextTick
+        throw e
+    },0)
+}
+
+// setup unhandled error handler
+// unhandled errors happen when done is called, and  then an exception is thrown from the future
+Future.error = function(handler) {
+    unhandledErrorHandler = handler
+}
+
+
+// instance methods
+
+// returns a value for the future (can only be executed once)
+// if there are callbacks waiting on this value, they are run in the next tick
+    // (ie they aren't run immediately, allowing the current thread of execution to complete)
+Future.prototype.return = function(v) {
+    resolve(this, 'return', v)
+}
+Future.prototype.throw = function(e) {
+    resolve(this, 'error', e)
+}
+function setNext(that, future) {
+    if((!future instanceof Future))
+        unhandledErrorHandler(Error("Value returned from then or catch *not* a Future: "+result))
+    resolve(that, 'next', future)
+}
+
+function wait(that, cb) {
+    if(that.resolved) {
+        executeCallbacks(that, [cb])
+    } else {
+        that.queue.push(cb)
+    }
+}
+
+function waitOnResult(f, result, cb) {
+    if(result !== undefined)
+        wait(result, function() {
+            if(this.hasError)
+                f.throw(this.error)
+            else {
+                try {
+                    setNext(f, cb(this.result))
+                } catch(e) {
+                    f.throw(e)
+                }
+            }
+        })
+    else
+        f.return(cb())
+}
+
+
+// cb takes one parameter - the value returned
+// cb can return a Future, in which case the result of that Future is passed to next-in-chain
+Future.prototype.then = function(cb) {
+    var f = new Future
+    wait(this, function() {
+        if(this.hasError)
+            f.throw(this.error)
+        else if(this.hasNext)
+            waitOnResult(f, this.next, cb)
+        else {
+            try {
+                setNext(f, cb(this.result))
+            } catch(e) {
+                f.throw(e)
+            }
+        }
+    })
+    return f
+}
+// cb takes one parameter - the error caught
+// cb can return a Future, in which case the result of that Future is passed to next-in-chain
+Future.prototype.catch = function(cb) {
+    var f = new Future
+    wait(this, function() {
+        if(this.hasError) {
+            try {
+                setNext(f, cb(this.error))
+            } catch(e) {
+                f.throw(e)
+            }
+        } else if(this.hasNext)
+            setNext(f, this.next)
+        else
+            f.return(this.result)
+    })
+    return f
+}
+// cb takes no parameters
+// callback's return value is ignored, but thrown exceptions propogate normally
+Future.prototype.finally = function(cb) {
+    var f = new Future
+    wait(this, function() {
+        try {
+            cb()
+            if(this.hasError)
+                f.throw(this.error)
+            else {
+                f.return(this.result)
+            }
+        } catch(e) {
+            f.throw(e)
+        }
+    })
+    return f
+}
+
+// all unused futures should end with this (e.g. most then-chains)
+// detatches the future so any propogated exception is thrown (so the exception isn't silently lost)
+Future.prototype.done = function() {
+    wait(this, function() {
+        if(this.hasError) {
+            unhandledErrorHandler(this.error)
+        }
+    })
+}
+
+
+
+Future.prototype.resolver = function() {
+    var me = this
+
+    return function(e,v) {
+        if(e) { // error argument
+            me.throw(e)
+        } else {
+            me.return(v)
+        }
+    }
+}
+
+Future.prototype.isResolved = function() {
+    return this.resolved
+}
+
+
+function resolve(that, type, value) {
+    if(that.resolved)
+        throw Error("Future resolved more than once! Resolution: "+value)
+
+    that.resolved = true
+    that.hasError = type === 'error'
+    that.hasNext = type === 'next'
+
+    if(that.hasError)
+        that.error = value
+    else if(that.hasNext)
+        that.next = value
+    else
+        that.result = value
+
+    executeCallbacks(that, that.queue)
+}
+
+function executeCallbacks(that, callbacks) {
+    if(callbacks.length > 0) {
+        setTimeout(function() {   // basically process.nextTick
+            callbacks.forEach(function(cb) {
+                cb.apply(that)
+            })
+        },0)
+    }
+}
---
+++ test/asyncFuturesTest.js
@@ -0,0 +1,237 @@
+"use strict";
+
+var Unit = require('deadunit')
+var Future = require('../asyncFuture')
+
+var futures = []
+var test = Unit.test("Testing async futures", function() {
+    var t = this
+    var countAsserts = 0
+    var expectedAsserts = 0
+
+    var f = new Future()
+    f.return(5)
+    f.then(function(v) {
+        t.ok(v===5)  // return before
+        countAsserts++
+    }).done()
+
+    var f2 = new Future()
+    f2.then(function(v) {
+        t.ok(v===6)  // return after
+        countAsserts++
+    }).done()
+    f2.return(6)
+
+    futures.push(f)
+    futures.push(f2)
+
+    expectedAsserts += 2
+
+    var f3
+    t.test("exceptions", function() {
+        var t = this
+
+        f3 = new Future()
+        f3.throw(Error("test1"))
+        f3.then(function(v) {
+            t.ok(false) // should never happen
+        }).catch(function(e) {
+            t.ok(e.message === "test1") // throw before
+            countAsserts++
+        }).done()
+
+        var f4 = new Future()
+        f4.then(function(v) {
+            t.ok(false) // should never happen
+        }).catch(function(e) {
+            t.ok(e.message === "test2")  // throw after
+            countAsserts++
+        }).done()
+        f4.throw(Error("test2"))
+
+        var f4 = new Future()
+        f4.then(function(v) {
+            throw Error("NOT OK")
+        }).catch(function(e) {
+            t.ok(e.message === 'NOT OK') // Throw inside then
+            countAsserts++
+        }).done()
+        f4.return("ok?")
+
+        var f4 = new Future()
+        f4.catch(function(e) {
+            throw "noooo"
+        }).catch(function(e) {
+            t.ok(e === 'noooo') // Throw inside catch
+            countAsserts++
+        }).done()
+        f4.throw("blah")
+
+        var f4 = new Future()
+        f4.finally(function(e) {
+            throw "nooootAgaaaaaaiin"
+        }).catch(function(e) {
+            t.ok(e === 'nooootAgaaaaaaiin') // Throw inside finally
+            countAsserts++
+        }).done()
+        f4.throw("blah")
+
+        // uncaught
+        Future.error(function(e) {
+            t.ok(e === "blah") // uncaught exception
+            countAsserts++
+            Future.error(function(e) {
+                t.log("Wtf: "+e)
+                t.ok(false)
+            })
+        })
+        var f4 = new Future()
+        f4.done()
+        f4.throw("blah")
+
+        expectedAsserts += 6
+        futures.push(f3)
+        futures.push(f4)
+    })
+
+    t.test("chaining", function() {
+        var t = this
+
+        var fs = [new Future, new Future, new Future]
+        fs[0].then(function(v) {
+            t.ok(v === 0)
+            countAsserts++
+            fs[1].return(1)
+            return fs[1]      // resolved before
+        }).then(function(v) {
+            t.ok(v === 1) // Chained after
+            countAsserts++
+            return fs[2]      // resolved after
+        }).then(function(v) {
+            t.ok(v === 2)
+            countAsserts++ // Chained before
+        }).done()
+        fs[0].return(0)
+        fs[2].return(2)
+
+        futures = futures.concat(fs)
+
+        expectedAsserts += 3
+    })
+
+
+    t.test("combining", function() {
+        var t = this
+
+        Future.all(f,f2).then(function(v){
+            t.ok(v[0] === 5) // ALL After Success
+            t.ok(v[1] === 6)
+            countAsserts+=2
+        }).done()
+
+        Future.all(f,f2,f3).then(function(v){
+            t.ok(false) // should never happen
+        }).catch(function(e) {
+            t.ok(e.message === 'test1') // ALL After Error
+            countAsserts++
+        }).done()
+
+        var f5 = new Future()
+        var f6 = new Future()
+        Future.all(f5, f6).then(function(v){
+            t.ok(v[0] === 'Ya') // ALL Before Success
+            t.ok(v[1] === 'ok')
+            countAsserts+=2
+        }).done()
+        f5.return("Ya")
+        f6.return("ok")
+
+        var f7 = new Future()
+        var f8 = new Future()
+        Future.all(f7, f8).then(function(v){
+            t.ok(false)// Shouldn't happen
+        }).catch(function(e) {
+            t.ok(e.message === 'err') // ALL Before error
+            countAsserts++
+        })
+        f7.return("Ya")
+        f8.throw(Error("err"))
+
+        futures.push(f6)
+        futures.push(f7)
+        futures.push(f8)
+
+        expectedAsserts += 6
+    })
+
+    t.test("working with callbacks", function(t) {
+        function asyncFn(cb) {
+            cb(undefined, "hi")
+        }
+        function asyncException(cb) {
+            cb(Error("callbackException"))
+        }
+
+        // resolver
+
+        var f9 = new Future
+        asyncFn(f9.resolver())
+        f9.then(function(x) {
+            t.ok(x === 'hi')
+            countAsserts++
+        })
+        futures.push(f9)
+
+        var f10 = new Future
+        asyncException(f10.resolver())
+        f10.catch(function(e) {
+            t.ok(e.message === 'callbackException')
+            countAsserts++
+        })
+        futures.push(f10)
+
+        // wrap
+
+        var f11 = Future.wrap(asyncFn)()
+        f11.then(function(x) {
+            t.ok(x === 'hi')
+            countAsserts++
+        })
+        futures.push(f11)
+
+        var f12 = Future.wrap(asyncException)()
+        f12.catch(function(e) {
+            t.ok(e.message === 'callbackException')
+            countAsserts++
+        })
+        futures.push(f12)
+
+        expectedAsserts += 4
+    })
+
+      /*
+
+    // longtraces
+    q.longStackSupport = true;
+    q.call(function() {
+        throw Error("test")
+    }).catch(function(e) {
+        console.log(e.stack)
+    }).fin(function() {
+        console.log("finally!")
+    })
+          */
+
+    var x = Future.all(futures)
+    futures.push(x)
+    x.finally(function() {
+        t.equal(countAsserts, expectedAsserts)
+    })
+})
+
+Future.all(futures).finally(function() {
+    test.writeConsole()
+})
+
+
