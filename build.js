93f1d0b0bf843cfbdd187fc5b7bcd34fc0e0935a
bt <fresheneesz@gmail.com>
2014-01-20 16:09:35 -0800

    fixing browser build

--- build.js
+++ build.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
-var build = require('buildModules')
+var build = require('build-modules')
 
 var buildDirectory = __dirname+'/generatedBuilds/'
 if(!fs.existsSync(buildDirectory)) {
@@ -10,5 +10,13 @@ var commonJsModule = fs.readFileSync(__dirname + '/asyncFuture.js').toString()
 var copywrite = '/* Copyright (c) 2013 Billy Tetrud - Free to use for any purpose: MIT License*/'
 
 console.log('building and minifying...')
-build(buildDirectory, 'asyncFuture', copywrite, commonJsModule, undefined)
-console.log('done')
+
+build(buildDirectory, 'asyncFuture', copywrite, __dirname + '/asyncFuture.js', undefined, function(e) {
+    if(e !== undefined) {
+        console.error(e.stack)
+        process.exit(1)
+    } else {
+        console.log('done')
+    }
+})
+
