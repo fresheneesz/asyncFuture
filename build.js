e32b62b6693f2ea062bffd8417701ef99aa8b875
bt <fresheneesz@gmail.com>
2014-02-20 00:59:13 -0800

    rm build from postinstall, cause postinstall is fucked in npm (see https://github.com/npm/npm/issues/4134 )

--- build.js
+++ build.js
@@ -1,22 +1,26 @@
 var fs = require('fs')
-var build = require('build-modules')
 
-var buildDirectory = __dirname+'/generatedBuilds/'
-if(!fs.existsSync(buildDirectory)) {
-    fs.mkdirSync(buildDirectory)
-}
+// atempting to work around npm bug https://github.com/npm/npm/issues/4134
+setTimeout(function() {
+    var build = require('build-modules')
 
-var commonJsModule = fs.readFileSync(__dirname + '/asyncFuture.js').toString()
-var copywrite = '/* Copyright (c) 2013 Billy Tetrud - Free to use for any purpose: MIT License*/'
-
-console.log('building and minifying...')
-
-build(buildDirectory, 'asyncFuture', copywrite, __dirname + '/asyncFuture.js', undefined, function(e) {
-    if(e !== undefined) {
-        console.error(e.stack)
-        process.exit(1)
-    } else {
-        console.log('done')
+    var buildDirectory = __dirname+'/generatedBuilds/'
+    if(!fs.existsSync(buildDirectory)) {
+        fs.mkdirSync(buildDirectory)
     }
-})
+
+    var copywrite = '/* Copyright (c) 2013 Billy Tetrud - Free to use for any purpose: MIT License*/'
+
+    console.log('building and minifying...')
+
+    build(buildDirectory, 'asyncFuture', copywrite, __dirname + '/asyncFuture.js', undefined, function(e) {
+        if(e !== undefined) {
+            console.error(e.stack)
+            process.exit(1)
+        } else {
+            console.log('done')
+        }
+    })
+}, 3000)
+
 
